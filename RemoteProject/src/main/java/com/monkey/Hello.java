c0978eccda511b450b2f50fbefbf2a40d4122cc1
dsharma <dsharma@test.com>
2013-06-15 15:54:53 +0530

    new project added

---
+++ RemoteProject/src/main/java/com/monkey/Hello.java
@@ -0,0 +1,18 @@
+package com.monkey;
+
+import java.util.UUID;
+
+/**
+ * Generate a unique number
+ *
+ */
+public class Hello
+{
+
+    public static void main( String[] args )
+    {
+               System.out.println("Hello");
+    }
+    
+
+}
